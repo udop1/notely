e9be34902f7a75c9a51debe04c3473b659d96dc8
S4101893 <93914871+S4101893@users.noreply.github.com>
2023-03-24 18:44:11 +0000

    Updated Todo
    Updated button to match the one in notes. So removed my Fixed Button jsx as it wasn't consistant. Took away the db connection for tonight as I broke it and ran out of time to find out why. So this is the current working state for the TODO list. I will reconnect the DB tommroow and intergrate the adding of the todos and the retrivals.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,53 @@
-import { Box, Container } from "@mui/material";
-import React from 'react';
+import { Box, Button, Container, Grid, SpeedDial, SpeedDialAction, SpeedDialIcon, Typography } from "@mui/material";
 import NavBar from "./NavBar";
+import ListRoundedIcon from '@mui/icons-material/ListRounded';
+import NoteAddOutlinedIcon from '@mui/icons-material/NoteAddOutlined';
+import { UserAuth } from "../context/AuthContext";
 
 const Todo = () => {
+    const { todo } = UserAuth();
+
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const today = new Date().toLocaleDateString('en-UK', options);
+    const split = today.split(" ");
+    // Date info
+    const stringify = split[0]+" "+split[1]+", "+split[2];
+
+    function handleClick(){
+        //Load Modal to add a task
+    }
+
     return (
         <Box>
             <NavBar />
+
+            <SpeedDial ariaLabel="SpeedDial" icon={<SpeedDialIcon />} sx={{ position: "absolute", bottom: 16, right: 16 }}>
+                <SpeedDialAction icon={<NoteAddOutlinedIcon />} tooltipTitle="New Note" sx={{ color: "black" }} onClick={handleClick}/>
+            </SpeedDial>
+
             <Container>
-                <div>To-do</div>
+                <div style={{ display:"flex", justifyContent: "flex-end"}}>
+                    <Button>Today</Button>
+                    <Button>Week</Button>
+                    <Button>Month</Button>
+                </div>
+                <Typography>Today</Typography>
+                <Typography>{stringify}</Typography>
+                
             </Container>
+            
+            <Container sx={{ mb: 2 }}>
+            <Grid container columns={12} justifyItems="center">
+                <Grid item xs={6}>
+                    <Button startIcon={<ListRoundedIcon />} sx={{ color: "black" }}><Typography variant="body1" sx={{ fontWeight: "700", color: "black" }}>To-DO</Typography></Button>
+                </Grid>
+            </Grid>
+        </Container>
+
+        <Container>
+            {/*List Current TODO for today*/}
+        </Container>
+
         </Box>
     );
 };
